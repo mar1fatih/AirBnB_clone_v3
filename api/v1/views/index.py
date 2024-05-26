5b0d9f343a6bdec4b79700b5ccdf6f9793815a68
mar1fatih <marouanefatih631@gmail.com>
2024-05-26 07:01:45 -0700

    index

---
+++ api/v1/views/index.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+a file index.py
+
+"""
+from flask import jsonify
+from api.v1.views import app_views
+from models import storage
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def show_status():
+    """show status"""
+    status = {
+            'status': 'ok'
+            }
+    return jsonify(status)
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def show_stats():
+    """show stats"""
+    stats = {
+    'amenities': storage.count('Amenity'),
+    'cities': storage.count('City'),
+    'places': storage.count('Place'),
+    'reviews': storage.count('Review'),
+    'states': storage.count('State'),
+    'users': storage.count('User')
+            }
+
+    return jsonify(stats)
